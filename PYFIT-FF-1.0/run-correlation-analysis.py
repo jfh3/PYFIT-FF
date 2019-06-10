37f33426bfc676dc846066735dd6a1641406900a
Adam Robinson <magnanimousllamacopter@gmail.com>
2019-06-10 13:49:05 -0400

    Added placeholder script

---
+++ PYFIT-FF-1.0/run-correlation-analysis.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import numpy             as np
+import sys
+import os
+import copy
+from   time import time
+
+if __name__ == '__main__':
+	# Here we will essentially be running pyfit in a loop
+	# with different command line parameters so that we can
+	# vary the target error for different structural groups.
+	pass
